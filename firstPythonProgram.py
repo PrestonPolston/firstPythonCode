ff01c55c77b82cc1489e25f2c5ac2f2aee537017
preston <polston13@outlook.com>
2024-09-21 19:43:59 -0400

    finished first user input fixed problem input initialy interpreted as str

---
+++ firstPythonProgram.py
@@ -0,0 +1,9 @@
+# I want to get two numbers from the user and use them in multiple math problems
+
+numberOne = int(input("Please enter your first number: "))
+
+numberTwo = int(input("Please enter your second number: "))
+
+sum = numberOne + numberTwo
+
+print("The sum of your numbers is ", sum)
